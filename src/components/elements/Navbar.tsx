560ce15805d30e9b9b2ecef0488b893b0536ea3c
Ramit Roshann <ramitroshan777777@gmail.com>
2025-06-12 20:38:09 +0530

    Your message about changes

---
+++ src/components/elements/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Container } from "../shared/Container";
+
+const navItems = [
+    {href: "#", text: "Home"},
+    {href: "services", text: "Services"},
+    {href: "about-us", text: "About Us"},
+    {href: "features", text: "Features"},
+];
+
+export const Navbar = () => {
+    return (
+        <header className="absolute inset-x-0 top-0 z-50 py-6">
+            <Container>
+                <nav className="w-full flex justify-between gap-6 relative">
+                    {/* Logo */}
+                    <div className="min-w-max inline-flex relative">
+                        <a>
+                            <img/>
+                        </a>
+                    </div>
+                </nav>
+            </Container>
+        </header>
+    );
+}
