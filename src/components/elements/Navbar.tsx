a5d12c7564b6cd1fb750c73baa896fd2425444a3
Ramit Roshann <ramitroshan777777@gmail.com>
2025-06-19 19:22:22 +0530

    Your message about changes

--- src/components/elements/Navbar.tsx
+++ src/components/elements/Navbar.tsx
@@ -2,6 +2,7 @@ import { Container } from "../shared/Container";
 import logo from "../../assets/icon.svg";
 import { NavItem } from "../shared/NavItem";
 import { BtnLink } from "../shared/BtnLink";
+import { useThemeStore } from "../../store/ThemeStore";
 
 const navItems = [
     {href: "#", text: "Home"},
@@ -11,6 +12,8 @@ const navItems = [
 ];
 
 export const Navbar = () => {
+    const {toggleTheme, theme} = useThemeStore();
+
     return (
         <header className="absolute inset-x-0 top-0 z-50 py-6">
             <Container>
@@ -47,7 +50,26 @@ export const Navbar = () => {
                     </div>
 
                     <div className="min-w-max flex items-center gap-x-3">
-                        <button className="outline-hidden flex relative text-heading-2 rounded-full p-2 lg:p-3 border border-box-border cursor-pointer">
+                        <button 
+                          onClick={toggleTheme} 
+                          className="outline-hidden flex relative text-heading-2 rounded-full p-2 lg:p-3 border border-box-border cursor-pointer"
+                        >
+                            {theme === "dark" ? (
+                              <svg
+                                xmlns="https://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                              >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                                />
+                              </svg>
+                            ): (
                             <svg
                                 xmlns="https://www.w3.org/2000/svg"
                                 fill="none"
@@ -61,7 +83,9 @@ export const Navbar = () => {
                                   strokeLinejoin="round"
                                   d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
                                 />
-                            </svg>
+                                </svg>
+                                )}
+                
                         </button>
                     </div>
                 </nav>
