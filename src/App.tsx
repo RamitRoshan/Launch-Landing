765b8eaf03e0fc277bb03cfba90320f6f7dc4fab
Ramit Roshann <ramitroshan777777@gmail.com>
2025-06-21 02:16:50 +0530

    makes multiple components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@
 import { Layout } from "./components/Layout";
 import { Brands } from "./components/sections/Brands";
 import { Hero } from "./components/sections/Hero";
+import { Services } from "./components/sections/Services";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Layout title="EdgeAI">
       <Hero/>
       <Brands/>
+      <Services/>
     </Layout>
   );
 }
